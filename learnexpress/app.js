75b21accec45c6c1c1712a3839f275b66132e413
abaozhizhong <904740350@qq.com>
2017-08-19 13:23:13 +0800

    by abaozhi

--- learnexpress/app.js
+++ learnexpress/app.js
@@ -1,25 +1,47 @@
 let express = require('express');
 let app = express();
+var body_parser = require('body-parser');
 let public_path = '/static';
-// let public_path = '';
 let fortnues = require('./lib/fortnues');
-
-app.disable('x-powered-by');
+app.disable('x-powered-by');//屏蔽响应头中存在express的信息
+app.use(body_parser())//POST中间件
 
 app.use('/static',express.static('public'))
 
 app.set('port',process.env.PORT||3000);
 
-let handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+let handlebars = require('express3-handlebars').create({defaultLayout:'main'});//一堆模板引擎的东西
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
+let goods = [
+    {
+        name:"我就是商品",
+        id:9527,
+        price:"1000"
+    },
+    {
+        name:"我就是商品",
+        id:9526,
+        price:"1000"
+    },
+]
 
+//提个一个个api
+app.get(public_path+'/api',function (request,respone) {
+    let a = {
+        age:19,
+        name:"abaozhi"
+    }
+    console.log(typeof request.query.blean); //返回来的数据是字符窜类型
+    if(Boolean(request.query.blean) == true){
+        respone.json(a)
+    }
+})
 
-//
-// app.get('/',function (req,res) {
-//     res.type('text/plain');
-//     res.send('这是/')
-// })
+//更行商品good
+app.put(public_path+'/put/:id',function(request,respone){
+        console.log(respone.params);
+})
 
 // app.get('/about',function(req,res){
 //     res.type('text/plain');
@@ -86,12 +108,29 @@ app.get(public_path+'/test',function (requset,respone) {
     respone.send('test')
 })
 
-//
+//从重定向redirect
+app.get(public_path+'/redirect',function (request,respone) {
+    respone.redirect(200,public_path+'/test')
+})
 
 app.get(public_path+"/error",function (request,res) {
     res.status(500);
     res.render('error');
 })
+
+app.get(public_path+'/form',function (request,respone) {
+    respone.render('form');
+})
+
+app.post(public_path+"/post",function (request,respone) {
+    console.log("表单提交");
+    console.log(request.query);
+    // console.log('query.form:'+respone.query.form);
+    console.log('body.v1:'+request.body.v1);
+    console.log('body.v3:'+request.body.v3);
+    console.log('body.v2:'+request.body.v2);
+})
+
 
 app.use((err,req,res,next)=> {
     console.error(err.stack);
